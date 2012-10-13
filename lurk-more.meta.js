ec30d9028e38763b99d602c5b8616ab6dbf7dc45
Stuart Baker <sdb@stuartdb.com>
2012-10-13 23:58:40 +1300

    Updated include link as the restriction was preventing open window function from being called

--- lurk-more.meta.js
+++ lurk-more.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name           	Lurk-More
-// @version        	0.1.0
+// @version        	0.1.1
 // @namespace      	https://github.com/stuartdb/lurk-more
 // @author         	Stuart Baker
 // @description    	Allows quickly opening your bookmarked threads which have new posts on SA.
-// @include        	*://forums.somethingawful.com/usercp.php
+// @include        	*://forums.somethingawful.com/usercp*
 // @grant   		none
 // @downloadURL    	https://github.com/stuartdb/lurk-more/raw/master/lurk-more.user.js
 // @updateURL    	https://github.com/stuartdb/lurk-more/raw/master/lurk-more.meta.js
