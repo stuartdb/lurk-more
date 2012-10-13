dc7834739c8498f54734090af4a929c24eba387a
Stuart Baker <sdb@stuartdb.com>
2012-10-14 00:22:11 +1300

    Increment version number in metadata

--- lurk-more.meta.js
+++ lurk-more.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           	Lurk-More
-// @version        	0.1.1
+// @version        	0.1.2
 // @namespace      	https://github.com/stuartdb/lurk-more
 // @author         	Stuart Baker
 // @description    	Allows quickly opening your bookmarked threads which have new posts on SA.
