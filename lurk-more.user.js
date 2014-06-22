2443b441e2874e7809034fcd2ad64e9b11cd08c2
Stuart Baker <sdb@stuartdb.com>
2014-06-22 15:49:37 +1200

    Remove meta file and update metadata block

--- lurk-more.user.js
+++ lurk-more.user.js
@@ -1,13 +1,12 @@
 // ==UserScript==
-// @name Lurk-More
-// @version 1.2.0
-// @namespace https://github.com/stuartdb/lurk-more
+// @name lurk-more
+// @version 1.2.1
+// @namespace http://stuartdb.com
 // @author Stuart Baker
 // @description Opens all bookmarked threads with new posts in new tabs
 // @include *://forums.somethingawful.com/usercp*
 // @grant GM_openInTab
 // @downloadURL https://github.com/stuartdb/lurk-more/raw/master/lurk-more.user.js
-// @updateURL https://github.com/stuartdb/lurk-more/raw/master/lurk-more.meta.js
 // @run-at document-end
 // ==/UserScript==
 
